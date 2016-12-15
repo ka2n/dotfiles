f314eaa89aeea323083cc88ef497e2dba712d760
Katsuma Ito <katsumai@gmail.com>
2016-12-15 11:58:22 +0900

    Latest install scripts for OSX.

--- install.sh
+++ install.sh
@@ -18,6 +18,9 @@ ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 export PATH=/usr/local/sbin:/usr/local/bin:$PATH
 export GOPATH=$HOME
 
+# Language Version
+curl -kL https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
+
 brew doctor
 brew update
 brew upgrade
@@ -32,12 +35,11 @@ brew install zsh --disable-etcdir
 brew install vim
 brew install tmux
 brew install git
-brew install hub
 brew install tig
 brew install go
 brew install python
-brew install python3
 brew install node
+brew install elixir
 brew install rbenv
 brew install ruby-build
 brew install nodenv
@@ -48,6 +50,8 @@ brew install libxml2
 brew install libxslt
 brew install imagemagick
 brew install ag
+brew install gh
+brew install ghq
 brew install jq
 brew install ngrep
 brew install autojump
@@ -55,30 +59,43 @@ brew install tree
 brew install heroku-toolbelt
 brew install gnu-sed
 brew install wget
-brew install wrk
 brew install reattach-to-user-namespace
 brew install brew-cask
 brew install ngrok
 brew install git-now
-brew install xcproj
+brew install git-secrets
 brew install pwgen
+brew install envchain
 
-brew install docker
-brew install dlite
-sudo dlite install && dlite start
-
+brew cask install visual-studio-code
+brew cask install xquartz
 brew cask install iterm2
+brew cask install appcode
 brew cask install google-japanese-ime
 brew cask install coteditor
 brew cask install firefox
 brew cask install google-chrome
-brew cask install keyremap4macbook
+brew cask install karabiner-elements
+brew cask install hammerspoon
 brew cask install screenhero
 brew cask install dropbox
+brew cask install spotify
+brew cask install alfred
+brew cask install evernote
+brew cask install yorufukurou
+brew cask install onyx
+brew cask install selfcontrol
 brew cask install onepassword
 brew cask install launchcontrol
 brew cask install imagealpha
+brew cask install wireshark
+brew cask install fabric
+brew cask install duet
 brew cleanup
+
+
+# Install from App Store
+# alternote
 
 
 # Ruby
@@ -100,3 +117,9 @@ go get -u github.com/gophertown/looper
 
 # Python
 pip2 install --upgrade -r ./requirement.txt
+brew cask install google-cloud-sdk
+
+# NodeJS
+npm install -g eslint_d
+npm install -g babel-eslint
+npm install -g eslint-plugin-react
