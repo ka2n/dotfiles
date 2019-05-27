596a352f3e9af7d3fd655559d32f3ceca4c9af10
Katsuma Ito <katsumai@gmail.com>
2019-05-27 12:30:25 +0900

    update

---
+++ bin-linux/setup-mouse.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Key repeat
+xset r rate 200 99
+
+#xinput set-prop 13 299 1
+
+# Trackpoint
+xinput --set-prop "pointer:TPPS/2 Elan TrackPoint" "libinput Accel Speed" 0.1
+xinput --set-prop "pointer:TPPS/2 Elan TrackPoint" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 0.5
+
+# Touchpad
+xinput --set-prop 'pointer:Synaptics TM3289-002' "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 0.5
+xinput --set-prop 'pointer:Synaptics TM3289-002' "libinput Accel Speed" -.1
+
+# MX Ergo
+xinput --set-prop "pointer:MX Ergo Mouse" 'libinput Accel Speed' 0
+xinput --set-prop "pointer:MX Ergo Mouse" 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1
