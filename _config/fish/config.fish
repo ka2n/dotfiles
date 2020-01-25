786719070a1669bb4d016851b1b3c24b9f380f9d
Katsuma Ito <katsumai@gmail.com>
2020-01-25 16:56:34 +0900

    fish: check the file/command exists before use it

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -33,51 +33,56 @@ set -U FZF_FIND_FILE_COMMAND "fd --type f . \$dir"
 set -U FZF_CD_COMMAND "fd --type d . \$dir"
 
 # gcloud
-switch (uname)
-  case Darwin
-      bass source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc'
-      set -x MANPATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/help/man /usr/local/share/man /usr/share/man /opt/x11/share/man
-  case Linux
-      source '/opt/google-cloud-sdk/path.fish.inc'
+if type -q gcloud
+    switch (uname)
+      case Darwin
+          bass source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc'
+          set -x MANPATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/help/man /usr/local/share/man /usr/share/man /opt/x11/share/man
+      case Linux
+          source '/opt/google-cloud-sdk/path.fish.inc'
+    end
 end
 
-function peco_z
-  set -l query (commandline)
+if type -q peco
+    function peco_z
+      set -l query (commandline)
 
-  if test -n $query
-    set peco_flags --query "$query"
-  end
+      if test -n $query
+        set peco_flags --query "$query"
+      end
 
-  z -l | peco $peco_flags | awk '{ print $2 }' | read recent
-  if [ $recent ]
-      cd $recent
+      z -l | peco $peco_flags | awk '{ print $2 }' | read recent
+      if [ $recent ]
+          cd $recent
+          commandline -r ''
+          commandline -f repaint
+      end
+    end
+
+    function urxvtc_window
+      set -l query (commandline)
+      if test -n $query
+          urxvtc -e $query
+      else
+          return
+      end
       commandline -r ''
       commandline -f repaint
-  end
+    end
+
+    function fish_user_key_bindings
+      bind \cr 'peco_select_history (commandline -b)'
+      bind \c] peco_select_ghq_repository
+      bind \co urxvtc_window
+      #bind \x1b peco_z
+      #bind \ett peco_todoist_item
+      #bind \etp peco_todoist_project
+      #bind \etl peco_todoist_labels
+      #bind \etc peco_todoist_close
+      #bind \etd peco_todoist_delete
+    end
 end
 
-function urxvtc_window
-  set -l query (commandline)
-  if test -n $query
-      urxvtc -e $query
-  else
-      return
-  end
-  commandline -r ''
-  commandline -f repaint
-end
-
-function fish_user_key_bindings
-  bind \cr 'peco_select_history (commandline -b)'
-  bind \c] peco_select_ghq_repository
-  bind \co urxvtc_window
-  #bind \x1b peco_z
-  #bind \ett peco_todoist_item
-  #bind \etp peco_todoist_project
-  #bind \etl peco_todoist_labels
-  #bind \etc peco_todoist_close
-  #bind \etd peco_todoist_delete
-end
 
 if type -q bitly
     function bitly
@@ -176,4 +181,6 @@ if type -q git-switch-trainer
     alias git="git-switch-trainer"
 end
 
-source $HOME/.local/share/fish/local.fish
+if test -e $HOME/.local/share/fish/local.fish
+    source $HOME/.local/share/fish/local.fish
+end
