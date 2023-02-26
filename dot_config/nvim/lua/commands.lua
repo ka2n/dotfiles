31d01035c123c97935e8bab211d9eeb232bd986a
Katsuma Ito <katsumai@gmail.com>
2023-02-27 06:56:25 +0900

    Update .config/nvim/lua/commands.lua

--- dot_config/nvim/lua/commands.lua
+++ dot_config/nvim/lua/commands.lua
@@ -3,8 +3,5 @@ vim.api.nvim_create_user_command('Prettier', "CocCommand prettier.forceFormatDoc
 vim.api.nvim_create_user_command('OR', "call CocActionAsync('runCommand', 'editor.action.organizeImport')", { nargs = 0 })
 
 -- autocmd
---  Highlight the symbol and its references when holding the cursor.
-vim.api.nvim_create_autocmd('CursorHold', {
-    pattern = { '*' },
-    command = "silent call CocActionAsync('highlight')",
-})
+vim.cmd("autocmd CursorHold * silent call CocActionAsync('highlight')")
+vim.cmd("autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')")
