68e2c8b6e3a9b79392275456cb9f60155cc8337b
Katsuma Ito <katsumai@gmail.com>
2024-07-30 18:54:37 +0900

    Update .local/bin/configure-webcam.sh

--- dot_local/bin/executable_configure-webcam.sh
+++ dot_local/bin/executable_configure-webcam.sh
@@ -2,4 +2,9 @@
 
 set -x
 
-v4l2-ctl --list-devices | grep -q video0 && v4l2-ctl -d /dev/video0 --set-ctrl power_line_frequency=1 || exit 0
+v4l2-ctl --list-devices | grep -q video0 \
+    && v4l2-ctl -d /dev/video0 \
+        -c power_line_frequency=1 \
+        -c auto_exposure=1 \
+        -c exposure_time_absolute=200 \
+    || exit 0
