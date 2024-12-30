815c9ad32908ad56b83f22ca5a22cdd9f39592b4
Katsuma Ito <katsumai@gmail.com>
2024-12-30 21:02:06 +0900

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -105,6 +105,17 @@ if type -q op
     end
 end
 
+if type -q yazi
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+end
+
 abbr --add golatest 'set -x GOTOOLCHAIN (curl -s -L "https://go.dev/VERSION?m=text" | head -n 1)+auto'
 
 # gcloud
