7dc12f3c145d2b19d304fa3a911bb63f38dbefa8
Katsuma Ito <katsumai@gmail.com>
2025-12-19 23:01:48 +0900

    Update .local/share/dark-mode.d/theme.sh

--- dot_local/share/dark-mode.d/executable_theme.sh
+++ dot_local/share/dark-mode.d/executable_theme.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-xfconf-query -c xsettings -p /Net/ThemeName -s "Nordic"
+dconf write /org/gnome/desktop/interface/gtk-theme '"Nordic"'
+dconf write /org/gnome/desktop/interface/color-scheme '"prefer-dark"'
